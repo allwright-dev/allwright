aeaff43c888a647540c1b70cc6ab39fe295037dc
Atmaram Naik <naik.atmaram@gmail.com>
2026-08-29 11:20:57 +0530

    chore: prepare release v0.0.50

--- go/bootstrap.go
+++ go/bootstrap.go
@@ -32,7 +32,7 @@ const (
 	allwrightRepositoryEnvVar  = "ALLWRIGHT_REPOSITORY"
 	allwrightVersionEnvVar     = "ALLWRIGHT_VERSION"
 	defaultReleaseRepository   = "allwright-dev/allwright"
-	defaultReleaseVersion      = "0.0.49"
+	defaultReleaseVersion      = "0.0.50"
 )
 
 var bootstrapState struct {
